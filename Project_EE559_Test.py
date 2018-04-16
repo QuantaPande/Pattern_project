75917aa7c961c2e9b0e5d87a9024e96f3ff9988a
QuantaPande <tan.pande@gmail.com>
2018-04-16 03:31:37 -0700

    Add files via upload
    Completed filling unknowns to the dataset

--- Project_EE559_Test.py
+++ Project_EE559_Test.py
@@ -1,101 +1,79 @@
-
-# coding: utf-8
-
-# In[52]:
-
-
 import pandas as ps
 import sklearn as skl
 import numpy as np
 import math
+from sklearn.preprocessing import OneHotEncoder
 
 def eucDist(vec_1,vec_2):
-    if(length(vec_1) != length(vec_2)):
+    if(len(vec_1) != len(vec_2)):
         raise ValueError("The dimensions of the two vectors must be the same")
-        break
     dist = 0    
-    for i in range(0, length(vec_1)):
+    for i in range(0, len(vec_1)):
         dist = dist + (vec_1[i] - vec_2[i])**2
     dist = math.sqrt(dist)
     return dist
 
-def kNearestNeighbours(train_knn, k):
-    for i in list(train_knn):
-        train_knn[:, i] = train_knn[:, i] - np.mean(train_knn[:, i])
-
-    dist = np.empty(shape = (np.shape(train_knn)[0], np.shape(train_knn)[0], 3))
+def kNearestNeighbours(train_knn, data_train, k):
+    dist = np.empty((data_train.shape[0], 2))
     index_i = 0
-    index_j = 0
-    for i in train_knn.index.tolist():
-        for j in train_knn.index.tolist():
-            dist[index_i][index_j][0] = eucDist(train_knn[i, :], train_knn[j, :])
-            dist[index_i][index_j][1] = i
-            dist[index_i][index_j][2] = j
-            index_j = index_j + 1
-        index_j = 0
+    for i in data_train.index.tolist():
+        dist[index_i][0] = eucDist(train_knn.values, data_train.loc[i, :].values)
+        dist[index_i][1] = i
         index_i = index_i + 1
+    dist = dist[dist[:, 0].argsort()]
+    dist_knn = list(dist[1:k+1, 1])
+    return dist_knn
 
-    for i in range(0, np.shape(dist)[0]):
-        train_knn[i, :, :] = train_knn[train_knn[i, :, 0].argsort()]
+    
+def fit_unknowns(data_train, known_index, unknwon_index, train_knn_enc, k):
+    for i in unknown_index:
+        knn_loc = kNearestNeighbours(train_knn_enc.loc[i, :], train_knn_enc.loc[known_index, :], k)
+        for j in list(data_train):
+            if data_train.loc[i, j] == 'NaN':
+                fitter_value = data_train.loc[knn_loc, j].mode().iloc[0]
+                data_train.loc[i, j] = fitter_value
+    return data_train
 
-    dist_knn = dist[1:k+1, :, 1:2]
     
-def fit_unknowns(data_x, known_index, fill_unknown, knn_loc):
-    
-    
-    
-data_x = ps.read_csv("bank-additional.csv")
-data_x['y_class'] = data_x['y'].apply(lambda x:0 if x == 'no' else 1)
-label_train = data_x['y_class']
-data_x.drop(['y_class'], axis = 1, inplace = True)
-data_x.drop(['y'], axis = 1, inplace = True)
-data_x = data_x.replace(to_replace = 'unknown', value = 'NaN')
-data_test = ps.DataFrame(columns = list(data_x))
-print(data_x.shape)
-
+data_train = ps.read_csv("bank-additional.csv")
+data_train['y_class'] = data_train['y'].apply(lambda x:0 if x == 'no' else 1)
+label_train = data_train['y_class']
+data_train.drop(['y_class'], axis = 1, inplace = True)
+data_train.drop(['y'], axis = 1, inplace = True)
+data_train = data_train.replace(to_replace = 'unknown', value = 'NaN')
+data_test = ps.DataFrame(columns = list(data_train))
 
 # In[53]:
 
-
-for i in range(0, data_x.shape[0]):
+for i in range(0, data_train.shape[0]):
     rand = np.random.random_sample()
     if(rand < 0.1):
-        data_test = data_test.append(data_x.loc[i, :])
-        data_x.drop(i, axis = 0)
-data_x.set_index(np.arange(0, data_x.shape[0]))
+        data_test = data_test.append(data_train.loc[i, :])
+        data_train.drop(i, axis = 0, inplace = True)
+data_train.set_index(np.arange(0, data_train.shape[0]))
 data_test.set_index(np.arange(0, data_test.shape[0]))
-#print(data_x)
-#print(data_test)
+print(data_train.shape)
+print(data_test.shape)
 
 train_knn = ps.DataFrame(columns = list(data_train))
-train_knn = train_knn.append(data_train.loc[0, :])
 fill_unknown = ps.DataFrame(columns = list(data_train))
-fill_unknown = fill_unknown.append(data_train.loc[0, :])
 known_index = []
-for i in range(1, data_train.shape[0]):
-    if (~data_train[i,:] == 'NaN'):
-        train_knn = train_knn.append(data_train.loc[i, :])
+unknown_index = []
+for i in data_train.index.tolist():
+    if all(data_train.loc[i, :] != 'NaN'):
         known_index.append(i)
     else:
-        fill_unknown = fill_unknown.append(data_train.loc[i, :])
+        unknown_index.append(i)
+    train_knn = train_knn.append(data_train.loc[i, :])
 
+print("DONE!!!! Split dataset")
+train_knn.drop(['job', 'marital', 'education', 'default', 'housing', 'loan'], axis = 1, inplace = True)
+print("DONE!!!! Dropped features")
+train_knn_enc = ps.get_dummies(train_knn, prefix = {'contact':'contacct', 'month':'month', 'day_of_week':'day_of_week', 'poutcome':'poutcome'}, columns=['contact', 'month', 'day_of_week', 'poutcome'])
+for i in list(train_knn_enc):
+    train_knn_enc.loc[:, i] = train_knn_enc.loc[:, i] - np.mean(train_knn_enc.loc[:, i])
 
-train_knn.drop('job', 'maital', 'education', 'default', 'housing', 'loan')
-fill_unknown.drop('job', 'maital', 'education', 'default', 'housing', 'loan')
+data_train = fit_unknowns(data_train, known_index, unknown_index, train_knn_enc, 5)
+print(data_train)
 
-ohe = OneHotEncoder()
-for i in range (1,4):
-    ohe.fit_transform(train_knn[:,i])
-    ohe.fit_transform(fill_unknown[:,i])
-ohe.fit_transform(train_knn[:,7])
-ohe.fit_transform(fill_unknown[:,7])
-
-train_knn.drop('contact', 'month', 'day_of_week', 'poutcome')
-fill_unknown.drop('contact', 'month', 'day_of_week', 'poutcome')
-
-
-train_knn = train_knn.values
-fill_unknown = fill_unknown.values
-
-knn_loc = kNearestNeighbours(train_knn, 5)
 
