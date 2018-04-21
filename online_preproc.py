b9b5736a60260bd7cbb6bc0fabb40bf10daa4752
chinmayjog13 <37167529+chinmayjog13@users.noreply.github.com>
2018-04-22 00:18:04 +0530

    Update online_preproc.py

--- online_preproc.py
+++ online_preproc.py
@@ -26,37 +26,40 @@ class preProc():
         self.data_test.set_index(np.arange(0, self.data_test.shape[0]))
         
         self.label_train = self.data_train['y']
-        self.data_train.drop(['y'], axis = 1, inplace = True)
         self.data_train.drop(['shares'], axis = 1, inplace = True)
+        self.data_train.to_csv("Online-data-train.csv", index = False)
+        self.data_train.drop(['y'], axis = 1, inplace = True)
         
         self.label_test = self.data_test['y']
         self.data_test.drop(['shares'], axis = 1, inplace = True)
+        self.data_test.to_csv("Online-data-test.csv", index = False)
         self.data_test.drop(['y'], axis = 1, inplace = True)
     
-    def normalize(self):    
+    def normalize(self, data_train, data_test):    
         self.category = ['data_channel_is_lifestyle', 'data_channel_is_entertainment', 
                               'data_channel_is_bus', 'data_channel_is_socmed', 
                               'data_channel_is_tech', 'data_channel_is_world', 
                               'weekday_is_monday', 'weekday_is_tuesday', 'weekday_is_wednesday', 
                               'weekday_is_thursday', 'weekday_is_friday', 'weekday_is_saturday', 
                               'weekday_is_sunday', 'is_weekend']
-        self.data_train_category = ps.DataFrame(columns = list(self.category))
-        self.data_train_category[self.category] = self.data_train[self.category]
+        data_train_category = ps.DataFrame(columns = list(self.category))
+        data_train_category[self.category] = data_train[self.category]
         
-        self.data_test_category = ps.DataFrame(columns = list(self.category))
-        self.data_test_category[self.category] = self.data_test[self.category]
+        data_test_category = ps.DataFrame(columns = list(self.category))
+        data_test_category[self.category] = data_test[self.category]
         
-        self.data_train.drop(self.category, axis = 1, inplace = True)
-        self.data_test.drop(self.category, axis = 1, inplace = True)
+        data_train.drop(self.category, axis = 1, inplace = True)
+        data_test.drop(self.category, axis = 1, inplace = True)
         
         self.scale = StandardScaler()
-        self.scale.fit(self.data_train.values[:,:])
-        self.scale.transform(self.data_train.values[:,:])
-        self.scale.transform(self.data_test.values[:,:])
+        self.scale.fit(data_train.values[:,:])
+        self.scale.transform(data_train.values[:,:])
+        self.scale.transform(data_test.values[:,:])
         
         
         #self.data_train = (self.data_train-self.data_train.mean(axis=0))/self.data_train.std(axis=0)
         #self.data_test = (self.data_test-self.data_train.mean(axis=0))/self.data_train.std(axis=0)
         
-        self.data_train[self.category] = self.data_train_category[self.category]
-        self.data_test[self.category] = self.data_test_category[self.category]
+        data_train[self.category] = data_train_category[self.category]
+        data_test[self.category] = data_test_category[self.category]
+        return data_train, data_test
